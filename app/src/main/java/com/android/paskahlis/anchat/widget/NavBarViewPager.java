6afadaccfbd38fc16269aa15c8acf546e4cb363a
Fajar Nugroho <fajar12.nugroho@gmail.com>
2018-02-22 16:07:00 +0700

    initialize main activity and fragments

---
+++ app/src/main/java/com/android/paskahlis/anchat/widget/NavBarViewPager.java
@@ -0,0 +1,62 @@
+package com.android.paskahlis.anchat.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Scroller;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by fajar on 22/02/18.
+ */
+
+public class NavBarViewPager extends ViewPager {
+    public NavBarViewPager(Context context) {
+        super(context);
+        setMyScroller();
+    }
+
+    public NavBarViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setMyScroller();
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        // Never allow swiping to switch between pages
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // Never allow swiping to switch between pages
+        return false;
+    }
+
+    //down one is added for smooth scrolling
+
+    private void setMyScroller() {
+        try {
+            Class<?> viewpager = ViewPager.class;
+            Field scroller = viewpager.getDeclaredField("mScroller");
+            scroller.setAccessible(true);
+            scroller.set(this, new MyScroller(getContext()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public class MyScroller extends Scroller {
+        public MyScroller(Context context) {
+            super(context, new DecelerateInterpolator());
+        }
+
+        @Override
+        public void startScroll(int startX, int startY, int dx, int dy, int duration) {
+            super.startScroll(startX, startY, dx, dy, 100 /*1 secs*/);
+        }
+    }
+}
